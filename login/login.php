edf3566f61d94207f54a05e1ab06541e68a8b98c
JuanIgnacioTosetti <132720472+JuanIgnacioTosetti@users.noreply.github.com>
2023-10-23 19:16:05 -0300

    Delete login directory

--- login/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo $_GET["nombre"];
-
-if ($_GET["nombre"] == "Juan" && $_GET["password"] == 1234){
-    print "<br> Se ingreso correctamente";
-} else { 
-    print "<br> Los datos ingresados son incorrectos";
-}
