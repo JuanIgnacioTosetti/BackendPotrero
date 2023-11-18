df92ce44719bb30db351645f5a78c6098c747b53
JuanIgnacioTosetti <132720472+JuanIgnacioTosetti@users.noreply.github.com>
2023-11-18 13:04:39 -0300

    Add files via upload

---
+++ cerveceria/tienda.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caracol Negro | Cerveceria Artesanal</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="shortcut icon" href="./img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+</head>
+<body style="background-color: #efefef;">
+   
+<?php include "./plantillas/navbar.php" ?>
+    <section class="tienda">
+        <h2 class="tienda__title">Nuestra Tienda</h2>
+        <div class="productos">
+            <!-- Producto 1 -->
+            <div class="card" style="width: 18rem;">
+                <img src="./img/1.jpg" class="card-img-top" alt="Product Image">
+                <div class="card-body">
+                  <h5 class="card-title">Nombre del Producto</h5>
+                  <p class="card-text">Precio: $XX.XX</p>
+                  <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+                </div>
+              </div>
+            <!-- Producto 2 -->
+            <div class="card" style="width: 18rem;">
+                <img src="./img/1.jpg" class="card-img-top" alt="Product Image">
+                <div class="card-body">
+                  <h5 class="card-title">Nombre del Producto</h5>
+                  <p class="card-text">Precio: $XX.XX</p>
+                  <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+                </div>
+              </div>
+            <!-- Producto 3 -->
+            <div class="card" style="width: 18rem;">
+                <img src="./img/1.jpg" class="card-img-top" alt="Product Image">
+                <div class="card-body">
+                  <h5 class="card-title">Nombre del Producto</h5>
+                  <p class="card-text">Precio: $XX.XX</p>
+                  <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+                </div>
+              </div>
+              <!-- Producto 4 -->
+            <div class="card" style="width: 18rem;">
+                <img src="./img/1.jpg" class="card-img-top" alt="Product Image">
+                <div class="card-body">
+                  <h5 class="card-title">Nombre del Producto</h5>
+                  <p class="card-text">Precio: $XX.XX</p>
+                  <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+                </div>
+              </div>
+            <div class="card" style="width: 18rem;">
+              <img src="./img/1.jpg" class="card-img-top" alt="Product Image">
+              <div class="card-body">
+                <h5 class="card-title">Nombre del Producto</h5>
+                <p class="card-text">Precio: $XX.XX</p>
+                <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="./img/1.jpg" class="card-img-top" alt="Product Image">
+                <div class="card-body">
+                  <h5 class="card-title">Nombre del Producto</h5>
+                  <p class="card-text">Precio: $XX.XX</p>
+                  <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="./img/1.jpg" class="card-img-top" alt="Product Image">
+                <div class="card-body">
+                  <h5 class="card-title">Nombre del Producto</h5>
+                  <p class="card-text">Precio: $XX.XX</p>
+                  <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="./img/1.jpg" class="card-img-top" alt="Product Image">
+                <div class="card-body">
+                  <h5 class="card-title">Nombre del Producto</h5>
+                  <p class="card-text">Precio: $XX.XX</p>
+                  <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+    
+
+    <!-- Botstrap -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!--  -->
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<script src="./js/swipe.js"></script>
+<script src="./js/openpopup.js"></script>
